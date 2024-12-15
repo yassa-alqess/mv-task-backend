22bce209227edb8383abe0227ffa1c9bc559b764
yassa-alqess <iscoadms2@gmail.com>
2024-12-15 02:55:45 +0200

    feat: update error messages in create-lead dto

--- src/modules/leads/leads.dto.ts
+++ src/modules/leads/leads.dto.ts
@@ -2,8 +2,12 @@ import Joi from 'joi';
 
 export const CreateLeadDto = Joi.object({
     fullName: Joi.string().max(200).required(),
-    email: Joi.string().email().max(200).regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/).required(),
-    phone: Joi.string().length(11).required(),
+    email: Joi.string().email().max(200)
+        .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/).message("email pattern is invalid")
+        .required(),
+    phone: Joi.string().length(11)
+        .regex(/^[0-9]+$/).message("phone pattern is invalid")
+        .required(),
     nid: Joi.string().length(14).required(),
 });
 
